2dc7e7e2f49e6169e7969bc4deb91c82dd0fb937
sgdavies <samgdavies@gmail.com>
2021-12-06 12:21:42 +0000

    Day 06 - java

---
+++ Day06.java
@@ -0,0 +1,27 @@
+public class Day06 {
+    public static void main(String[] args) {
+	final int end = Integer.parseInt(args[0]);
+	String test = "3,4,3,1,2";
+	String data = "1,1,1,1,1,1,1,4,1,2,1,1,4,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,5,1,1,1,1,3,1,1,2,1,2,1,3,3,4,1,4,1,1,3,1,1,5,1,1,1,1,4,1,1,5,1,1,1,4,1,5,1,1,1,3,1,1,5,3,1,1,1,1,1,4,1,1,1,1,1,2,4,1,1,1,1,4,1,2,2,1,1,1,3,1,2,5,1,4,1,1,1,3,1,1,4,1,1,1,1,1,1,1,4,1,1,4,1,1,1,1,1,1,1,2,1,1,5,1,1,1,4,1,1,5,1,1,5,3,3,5,3,1,1,1,4,1,1,1,1,1,1,5,3,1,2,1,1,1,4,1,3,1,5,1,1,2,1,1,1,1,1,5,1,1,1,1,1,2,1,1,1,1,4,3,2,1,2,4,1,3,1,5,1,2,1,4,1,1,1,1,1,3,1,4,1,1,1,1,3,1,3,3,1,4,3,4,1,1,1,1,5,1,3,3,2,5,3,1,1,3,1,3,1,1,1,1,4,1,1,1,1,3,1,5,1,1,1,4,4,1,1,5,5,2,4,5,1,1,1,1,5,1,1,2,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,5,1,1,1,1,1,1,3,1,1,2,1,1";
+
+	System.out.println(simulate(test, end));
+	System.out.println(simulate(data, end));
+    }
+
+    static long simulate(String input, int end) {
+	long[] fish = new long[end + 1 + 8];
+	for (String f : input.split(",")) {
+		int d = Integer.parseInt(f);
+		fish[d] += 1;
+	}
+	for (int day = 0; day < end; day++) {
+		fish[day+7] += fish[day];
+		fish[day+1+8] += fish[day];
+	}
+	long sum = 0;
+	for (int i=0; i<9; i++) {
+		sum += fish[end+i];
+	}
+	return sum;
+    }
+}
--- README.md
+++ README.md
@@ -6,3 +6,4 @@ A series of hacky solutions to this year's Advent of Code (https://adventofcode.
 - Day 03 - open 03.html and press "solve"
 - Day 04 - `python 04.py 04.dat`
 - Day 05 - `python 05.py 05.dat`
+- Day 06 - `javac Day06.java; java Day06 <limit>` (`limit` is `80` or `256`)
