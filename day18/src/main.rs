45b5218085192da725c8334cc9e1501bbb6fd6a6
Sam Davies <samgdavies@gmail.com>
2021-12-18 20:54:23 +0000

    day 18 - skeleteon reduce

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -63,6 +63,44 @@ impl Snailfish {
     fn add(left: Snailfish, right: Snailfish) -> Snailfish {
         Snailfish { left: Box::new(Element::Snailfish(left)), right: Box::new(Element::Snailfish(right)) }
     }
+
+    fn reduce(&mut self) {
+        loop {
+            if let Some(_explode) = self.explode() {
+                //
+            } else if let Some(_split) = self.split() {
+                //
+            } else {
+                // no more reducing to do
+                return;
+            }
+        }
+    }
+
+    fn explode(&mut self) -> Option<()> {
+        if let Some(e_left) = Snailfish::explode_element(&*self.left, 1) {
+            self.left = Box::new(e_left);
+            Some(())
+        } else if let Some(e_right) = Snailfish::explode_element(&*self.right, 1) {
+            self.right = Box::new(e_right);
+            Some(())
+        } else {
+            None
+        }
+    }
+
+    fn explode_element(elem: &Element, _depth: u8) -> Option<Element> {
+        match elem {
+            Element::Snailfish(_snail) => {
+                Some(Element::Regular(77))
+            },
+            Element::Regular(_) => None,
+        }
+    }
+
+    fn split(&self) -> Option<()> {
+        None
+    }
 }
 
 impl fmt::Display for Snailfish {
@@ -87,5 +125,10 @@ fn main() {
     println!("{}", Snailfish::parse_str(&"[1,[5,6]]"));
     println!("{}", Snailfish::parse_str(&"[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"));
     // println!("{}", Snailfish::parse_str(&"1,2]"));
-    println!("{}", Snailfish::add(Snailfish::parse_str(&"[1,2]"), Snailfish::parse_str(&"[[3,4],5]")));
+    let snail_one = Snailfish::parse_str(&"[1,2]");
+    let snail_two = Snailfish::parse_str(&"[[3,4],5]");
+    let mut snail_one = Snailfish::add(snail_one, snail_two);
+    println!("{}", snail_one);
+    snail_one.reduce();
+    println!("{}", snail_one);
 }
