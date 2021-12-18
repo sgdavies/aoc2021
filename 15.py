eed4da6cd4de9ce986dcade3137207e6c9eeefcc
Sam Davies <samgdavies@gmail.com>
2021-12-18 21:00:16 +0000

    profiling for day15

--- 15.py
+++ 15.py
@@ -6,48 +6,36 @@ with open(sys.argv[1]) as f:
 width = len(mapp[0].strip())
 depth = len(mapp)
 
-# part one
-costs = { (0,0): 0 }
-active = set([(0,0)])
+def solve(tiles):
+    TILES = tiles 
+    costs = { (0,0): 0 }
+    active = set([(0,0)])
 
-while active:
-    nx,ny = active.pop()
-    linked_nodes = [(nx-1,ny),(nx+1,ny),(nx,ny-1),(nx,ny+1)]
-    for node in linked_nodes:
-        x,y = node
-        if x<0 or x>=width or y<0 or y>=depth: continue
+    while active:
+        nx,ny = active.pop()
+        linked_nodes = [(nx-1,ny),(nx+1,ny),(nx,ny-1),(nx,ny+1)]
+        for node in linked_nodes:
+            x,y = node
+            if x<0 or x>=TILES*width or y<0 or y>=TILES*depth: continue
 
-        new_cost = costs[(nx,ny)] + int(mapp[y][x])
-        old_cost = costs.get((x,y))
-        if old_cost is None or new_cost < old_cost:
-            costs[(x,y)] = new_cost
-            active.add((x,y))
+            map_x = x%width
+            wrap_x = x//width
+            map_y = y%depth
+            wrap_y = y//depth
+            enter_cost = int(mapp[map_y][map_x]) + wrap_x + wrap_y
+            if enter_cost > 9: enter_cost -= 9
+            new_cost = costs[(nx,ny)] + enter_cost
+            #old_cost = costs.get((x,y))  ## slower that `x not in d or d[x]`
+            if (x,y) not in costs or new_cost < costs[(x,y)]:
+                costs[(x,y)] = new_cost
+                active.add((x,y))
 
-print(costs[(width-1, depth-1)])
+    return costs[(TILES*width-1, TILES*depth-1)]
 
-# part two
-TILES = 5
-costs = { (0,0): 0 }
-active = set([(0,0)])
+print(solve(1))
 
-while active:
-    nx,ny = active.pop()
-    linked_nodes = [(nx-1,ny),(nx+1,ny),(nx,ny-1),(nx,ny+1)]
-    for node in linked_nodes:
-        x,y = node
-        if x<0 or x>=TILES*width or y<0 or y>=TILES*depth: continue
-
-        map_x = x%width
-        wrap_x = x//width
-        map_y = y%depth
-        wrap_y = y//depth
-        enter_cost = int(mapp[map_y][map_x]) + wrap_x + wrap_y
-        if enter_cost > 9: enter_cost -= 9
-        new_cost = costs[(nx,ny)] + enter_cost
-        old_cost = costs.get((x,y))
-        if old_cost is None or new_cost < old_cost:
-            costs[(x,y)] = new_cost
-            active.add((x,y))
-
-print(costs[(TILES*width-1, TILES*depth-1)])
-
+if len(sys.argv) > 2 and sys.argv[2] == "--profile":
+    import cProfile
+    cProfile.run("print(part_two())")
+else:
+    print(solve(5))
